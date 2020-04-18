4ee19a3aff8ea19ddbb681977af1c434df4c390f
Tcreamer <trentoncreamer@yahoo.com>
2020-04-18 09:44:59 -0700

    Homework working

--- script.js
+++ script.js
@@ -2,17 +2,22 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
+function writePassword() {
+  var password = generatePassword(options);
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
+
 
 }
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+generateBtn.addEventListener("click", function() {
+  writePassword();
+
+});
+
 
 // Steps to make randon # generator
 
